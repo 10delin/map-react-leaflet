4d0dcf6e52557a4443e1486108ab5c1b63c8841e
Miguel Martin <miguel.martin@unitedcuisines.com>
2022-04-27 14:00:19 +0200

    Start adding filters

--- src/Components/MyMap.js
+++ src/Components/MyMap.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import L from "leaflet";
 import {
   LayersControl,
@@ -16,7 +16,7 @@ function GetIcon(iconSize, iconColor) {
     popupAnchor: [1, -34],
   });
 }
-const locations = [
+const data = [
   {
     key: 1,
     name: "Digital Cuisine",
@@ -43,36 +43,65 @@ const locations = [
   },
 ];
 
-function MyMap() {
-  //const filteredData = locations.filter((loc) => loc.type === "Trabajo");
-  //const types = ["Colegios", "Trabajos"];
+const getUniqueFilterItems = (locations) => {
+  const uniqueFilters = []
+  locations.map(location => {
+    if(!uniqueFilters.includes(location.type)) {
+      uniqueFilters.push(location.type)
+    }
+  })
+
+  return uniqueFilters
+}
+
+const MyMap = () => {
+  const [locations, setLocations] = useState([])
+  const [filterItems, setFilterItems] = useState(getUniqueFilterItems[locations])
+
+  useEffect(() => {
+    setLocations(data)
+  },[])
 
   return (
-    <MapContainer
-      className="map"
-      center={[37.22468458759511, -4.701167986858217]}
-      zoom={8}
-      style={{ height: 800, width: "100%" }}
-    >
-      <TileLayer
-        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
-      />
+    <>
+      <MapContainer
+        className="map"
+        center={[37.22468458759511, -4.701167986858217]}
+        zoom={8}
+        style={{ height: 800, width: "100%" }}
+      >
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
+        />
 
-      <LayersControl collapsed={false} position="bottomright">
-        <LayersControl.Overlay checked name="Colegios">
-          {locations.map((location) => (
-            <Marker
-              key={location.key}
-              position={location.position}
-              icon={GetIcon(location.size, location.iconColor)}
-            >
-              <Popup>{location.name}</Popup>
-            </Marker>
-          ))}
-      </LayersControl.Overlay>  
-      </LayersControl>
-    </MapContainer>
+        <LayersControl collapsed={false} position="bottomright">
+          <LayersControl.Overlay checked name="Colegios">
+            {locations.map((location) => (
+              <Marker
+                key={location.key}
+                position={location.position}
+                icon={GetIcon(location.size, location.iconColor)}
+              >
+                <Popup>{location.name}</Popup>
+              </Marker>
+            ))}
+        </LayersControl.Overlay>  
+        </LayersControl>
+      </MapContainer>
+      <div>
+        {
+          locations.map((location, index) => {
+            return (
+              <div key={index}>
+                <input type="checkbox"/>
+                <label>{location.type}</label>
+              </div>
+            )
+          })
+        }
+      </div>
+    </>
   );
 }
 
